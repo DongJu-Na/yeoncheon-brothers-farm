ca1541dab4e84b79a6e89f97ef88925aadeaa7e5
DongJu-Na <ehdeld123@naver.com>
2025-04-14 09:26:07 +0900

    gh-page settings

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import FeaturesPage from './pages/FeaturesPage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 
 function App() {
+  
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
